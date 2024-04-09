241afe3a2259621c96294043d715a66fde693930
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2024-04-09 19:59:41 -0300

    add

---
+++ clase_1.py
@@ -0,0 +1,53 @@
+# Variables
+nombre = "Juan"
+resultado = 2 + 2
+
+
+# Constante
+VALOR = 10
+
+# Numero enteros
+numero = 10
+
+# Numeros decimales
+numero_decimal = 10.4
+
+# Cadena de texto
+cadena_texto = "Av siempre viva 123"
+cadena_texto2 = 'nombre'
+
+# Boolean
+verdadero = True
+falso = False
+
+binario = b'hola'
+
+# Valor Nada
+nada = None
+
+# Tipos de datos complejos
+from datetime import datetime
+fecha = datetime.now()
+print(fecha)
+
+
+# Retomamos variables
+# Reglas de las variables
+# No pueden comenzar con un numero
+# Solo pueden usar el _ como caracter especial
+
+# PEP-8 regla de como escribir codigo
+# PEP-XXXXXX
+
+# Reglas
+
+# Las variables van siempre en minuscula (snake case)
+# El nombre en las variables se va separar con _
+# Ej mensaje_inicio_curso
+
+# La nomenclatura de las funciones es lower snake case
+# def calcular_posicion
+
+# Cuando usamos upper camel case:
+# Cuando creamos clases Auto, Perro, AutoDeportivo, CalcularPosicion
+
---
+++ clase_3.py
@@ -0,0 +1,124 @@
+# Condicionales (if elif else)
+
+variable1 = True
+variable2 = False
+
+# if variable1:
+#     print('variable 1')
+
+# if not variable2:
+#     print('variable 2')
+
+if not variable2:
+    print('variable 2') # Esta
+elif not variable2:
+    print('variable 2')
+elif variable1:
+    print('variable 1')
+elif variable1:
+    print('variable 1')
+elif variable1:
+    print('variable 1')
+elif variable1:
+    print('variable 1')
+elif variable1:
+    print('variable 1')
+else:
+    print('Ninguno de los anteriores')
+
+if True:
+    pass
+
+
+
+if False:
+    pass
+elif True:
+    pass
+
+
+
+if False:
+    pass
+elif False:
+    pass
+else:
+    pass
+
+
+if False:
+    pass
+else:
+    pass
+
+
+if True:
+    pass
+elif False:
+    pass
+elif False:
+    pass
+else:
+    pass
+
+
+if True:
+    pass
+elif False:
+    pass
+elif False:
+    pass
+
+
+
+if True:
+    pass
+elif False:
+    pass
+elif False:
+    pass
+
+
+
+print('Final del código')
+
+if variable1 == False:
+    pass
+
+manzanas = 0
+
+if manzanas == 0:
+    pass
+
+
+if manzanas:
+    pass
+elif manzanas == 5:
+    pass
+else:
+    pass
+
+
+
+# DOCString
+"""
+OR:
+0 + 0 = 0
+0 + 1 = 1
+1 + 0 = 1
+1 + 1 = 1
+
+
+AND:
+0 * 0 = 0
+1 * 0 = 0
+0 * 1 = 0
+1 * 1 = 1
+
+
+((True and False) and (False or True)) or True
+      *         *        +       +
+True and False and False or True or True
+
+"""
+
